f61f448dc8123c33852b07df1b2e04d78e7a7a6a
Kailey Liou <kaileyliou09@gmail.com>
2025-08-06 13:36:00 -0400

    added images

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -9,13 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.systemGreen)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .center, spacing: 20.0) {
+                Text("Kailey")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(hue: 0.363, saturation: 0.091, brightness: 0.976))
+                    .multilineTextAlignment(.center)
+                
+                Image("kailey")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                
+                Text("Kode With Klossy Scholar")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.white)
+                
+                Image("cookies")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                
+                Text("I love desserts!")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.white)
+                
+                Image("bubbletea")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                
+                Text("I also love bubble tea!")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.white)
+                
+                Image("broadway")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                
+                Text("I enjoy watching Broadway shows")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.white)
+    
+            }
+
         }
         .padding()
+
     }
 }
 
