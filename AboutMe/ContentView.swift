412eff2a0e5e8818b6011053eaeb248b2165c4a9
KaileyLiou <kaileyliou09@gmail.com>
2025-08-06 22:23:43 -0400

    fixed images and added scroll

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -12,61 +12,77 @@ struct ContentView: View {
         ZStack {
             Color(.systemGreen)
                 .ignoresSafeArea()
-            
-            VStack(alignment: .center, spacing: 20.0) {
-                Text("Kailey")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color(hue: 0.363, saturation: 0.091, brightness: 0.976))
-                    .multilineTextAlignment(.center)
-                
-                Image("kailey")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .cornerRadius(15)
-                
-                Text("Kode With Klossy Scholar")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .foregroundColor(Color.white)
-                
-//                Image("cookies")
-//                    .resizable()
-//                    .aspectRatio(contentMode: .fit)
-//                    .cornerRadius(15)
-//                
-//                Text("I love desserts!")
-//                    .font(.title2)
-//                    .fontWeight(.semibold)
-//                    .foregroundColor(Color.white)
-                
-                Image("bubbletea")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .cornerRadius(15)
-                
-                Text("I love bubble tea!")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .foregroundColor(Color.white)
-                
-                Image("broadway")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .cornerRadius(15)
-                
-                Text("I enjoy watching Broadway shows")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .foregroundColor(Color.white)
-    
-            }
 
+            ScrollView {
+                
+                VStack(alignment: .center, spacing: 20.0) {
+                    Text("Kailey")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(hue: 0.363, saturation: 0.091, brightness: 0.976))
+                        .multilineTextAlignment(.center)
+                    
+                        .padding()
+                    
+                    Image("kailey")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(15)
+                        .frame(width: 300, height: 300)
+                    
+                    Text("Kode With Klossy Scholar")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.white)
+                    
+                        .padding()
+                    
+                    Image("cookies")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(15)
+                        .frame(width: 250, height: 270)
+                    
+                    Text("I love desserts!")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.white)
+                    
+                        .padding()
+                    
+                    Image("bubbletea")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(15)
+                        .frame(width: 250, height: 270)
+                    
+                    Text("I also love bubble tea!")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.white)
+                    
+                        .padding()
+                    
+                    Image("broadway")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(15)
+                        .frame(width: 250, height: 270)
+                    
+                    Text("I enjoy watching Broadway shows")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.white)
+                    
+                        .padding()
+                    }
+                    
+                }
+                
+            }
         }
-//        .padding()
 
     }
-}
 
 #Preview {
     ContentView()
